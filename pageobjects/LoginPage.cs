7b7d66ebbc4f29bcfd368e39e8badd5db23ae095
Alain <nopersona@gmail.com>
2018-06-15 02:20:29 -0700

    initial commit

---
+++ pageobjects/LoginPage.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using OpenQA.Selenium;
+
+
+namespace _3dLoginTest.PageObjects
+{   
+    class LoginPage
+    {
+
+        private IWebDriver driver;
+
+        public LoginPage(IWebDriver driver)
+        {
+            this.driver = driver;
+        }
+
+        IWebElement LoginEmail => driver.FindElement(By.CssSelector("input[data-test='login-email']"));
+        IWebElement LoginPassword => driver.FindElement(By.CssSelector("input[data-test='login-password']"));
+        IWebElement LoginError => driver.FindElement(By.CssSelector("div[data-test='login-error']"));
+        
+        public void LoginApp(string userName, string password)
+        {
+            LoginEmail.SendKeys(userName);
+            LoginPassword.SendKeys(password);
+            LoginPassword.SendKeys(Keys.Enter);
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+        }
+
+        public string GetDisplayedError()
+        {
+            return LoginError.Text;
+        }
+    }
+}
+
---
+++ tests/LoginTest.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using _3dLoginTest.PageObjects;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium;
+
+namespace _3dLoginTest
+{
+    [TestClass]
+    public class LoginTest
+    {
+        IWebDriver driver;
+        LoginPage page;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            driver = new ChromeDriver(@"C:\Users\Alain\Desktop\Selenium\bin\Debug\netcoreapp2.1");
+            driver.Navigate().GoToUrl("https://sitescan-staging.3dr.com/auth");
+            driver.Manage().Window.Maximize();
+        }
+
+        [TestMethod]
+        public void LoginTest1()
+        {
+            page = new LoginPage(driver);
+            page.LoginApp("simple@test.com", "jude");
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(5);
+            Assert.AreEqual("Incorrect username or password",page.GetDisplayedError());
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            driver.Close();
+        }
+    }
+}
